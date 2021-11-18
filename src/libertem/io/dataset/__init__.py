cfc560bb3149977e2400f4970db8b204d9b1e550
Matthew Bryan <matthewbryan52@gmail.com>
2021-11-18 10:30:53 +0100

    Add DM4DataSet as loadable

--- src/libertem/io/dataset/__init__.py
+++ src/libertem/io/dataset/__init__.py
@@ -19,6 +19,7 @@ filetypes = {
     "dm": "libertem.io.dataset.dm.DMDataSet",
     "seq": "libertem.io.dataset.seq.SEQDataSet",
     "mrc": "libertem.io.dataset.mrc.MRCDataSet",
+    "dm4": "libertem.io.dataset.dm4file.DM4DataSet"
 }
 
 
