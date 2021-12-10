09f9daf7b4cfbb98ee84887abfcdda69c6fa61a6
Matthew Bryan <matthewbryan52@gmail.com>
2021-12-10 10:10:15 +0100

    Set udf reference on executor

--- src/libertem/udf/base.py
+++ src/libertem/udf/base.py
@@ -1807,6 +1807,7 @@ class UDFRunner:
         self._debug_task_pickling(tasks)
 
         executor = executor.ensure_sync()
+        executor.register_master_udfs(self._udfs)
 
         try:
             if progress:
